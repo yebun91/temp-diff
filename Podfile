333309ebdefaf378e247a55b77f3a56c3aa83729
yebun91 <65695703+yebun91@users.noreply.github.com>
2024-05-11 19:40:49 +0900

    빌드까지 성공

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '16.0'
+
+target 'temp-diff' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for temp-diff
+  pod 'Google-Mobile-Ads-SDK', '~> 11.4.0'
+
+end
--- temp-diff/ContentView.swift
+++ temp-diff/ContentView.swift
@@ -4,18 +4,47 @@
 //
 //  Created by 최유진 on 5/11/24.
 //
-
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject var weatherKitManager = WeatherKitManager()
+    @StateObject var locationDataManager = LocationDataManager()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            VStack(alignment: .leading) {
+                TopBarView()
+                CurrentTempView()
+                WeatherIndicatorView()
+                HourlyForecastScrollView()
+                AdView().frame(height: 60)
+            }.padding()
+
+            if weatherKitManager.isLoading {
+                // 로딩 화면 구현
+                Color.black.opacity(0.5)
+                    .ignoresSafeArea() // 화면 전체를 덮습니다.
+                Text("Loading...")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+            }
         }
-        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        // 사용자가 앱을 시작할 때와 앱으로 다시 돌아올 때 모두 최신 날씨 정보를 불러옴.
+        .onAppear {
+            Task {
+                await weatherKitManager.getWeathersFromYesterdayToTomorrow(latitude: locationDataManager.latitude, longitude: locationDataManager.longitude)
+                     }
+                 }
+        .task{
+            locationDataManager.weatherKitManager = weatherKitManager // 인스턴스를 공유합니다.
+            await weatherKitManager.getWeathersFromYesterdayToTomorrow(latitude: locationDataManager.latitude, longitude: locationDataManager.longitude)
+        }
+        .environmentObject(weatherKitManager)
+        .environmentObject(locationDataManager)
+        .background(Color("backgraund"))
+        
     }
 }
 
